022e975af3e03c0cc7c62b86fce611c39b49273f
Brendan Funk <brendanzfunk@gmail.com>
2023-07-14 15:16:03 -0700

    added custom font and created nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <p className="title">Fighting Game Trainer</p>
+          <div className="profileOptions">
+            <p>Log In</p>
+            <p>Sign Up</p>
+          </div>
+        </nav>
       </header>
     </div>
   );
